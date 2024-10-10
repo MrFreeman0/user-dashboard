8cebc74356bec730988800ef8050544f0d2c8e0b
Ilia Maslovskii <32616894+MrFreeman0@users.noreply.github.com>
2024-10-10 13:52:30 +0300

    Add state elements to dashboard
    Add conditional rendering based on the request state

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,7 +1,21 @@
-const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
-}
+import ErrorDashboard from "./elements/ErrorDashboard";
+import LoadedDashboard from "./elements/LoadedDashboard";
+import LoadingDashboard from "./elements/LoadingDashboard";
+import useDashboardData from "./hooks/useDashboardData";
 
-export default DashboardPage
+const DashboardPage = () => {
+  // Hooks
+  const { userData, isLoading, error, refreshData } = useDashboardData;
+
+  return (
+    <div>
+      {isLoading ? (
+        <LoadingDashboard />
+      ) : (
+        <>{error !== undefined ? <ErrorDashboard /> : <LoadedDashboard />}</>
+      )}
+    </div>
+  );
+};
+
+export default DashboardPage;
