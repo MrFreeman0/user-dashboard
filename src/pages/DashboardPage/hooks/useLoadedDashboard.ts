e0c87e547c25ac50eed045d24c58be1a9b163b79
Ilia Maslovskii <32616894+MrFreeman0@users.noreply.github.com>
2024-10-10 13:59:53 +0300

    Add state and retain data to presentation hook

--- src/pages/DashboardPage/hooks/useLoadedDashboard.ts
+++ src/pages/DashboardPage/hooks/useLoadedDashboard.ts
@@ -1,9 +1,22 @@
+import { useState, useMemo } from "react";
 import INormalisedData from "../../../interfaces/INormsalisedData";
+import { ISortState } from "../../../interfaces/ISortInterface";
 import TNormalisedUsers from "../../../interfaces/TNormalisedUsers";
 
 const useLoadedDashboard = (
   userData: INormalisedData<TNormalisedUsers, number>
 ) => {
+  // State
+  const [query, setQuery] = useState<string>("");
+  const [sortState, setSortState] = useState<ISortState>({
+    sortWith: "name",
+    sortBy: "desc",
+  });
+
+  const normalisedUsers = useMemo(() => {
+    return { ...userData };
+  }, [userData]);
+  
   return {};
 };
 
