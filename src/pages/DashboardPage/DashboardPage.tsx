1ef50c37b34c0e175ebeb55e7b46d41ec4675f23
Ilia Maslovskii <32616894+MrFreeman0@users.noreply.github.com>
2024-10-10 16:06:40 +0300

    Add animations for transitions, fix ref bug

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -2,25 +2,50 @@ import ErrorDashboard from "./elements/ErrorDashboard";
 import LoadedDashboard from "./elements/LoadedDashboard";
 import LoadingDashboard from "./elements/LoadingDashboard";
 import useDashboardData from "./hooks/useDashboardData";
+import "./DashboardPage.scss";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { useRef } from "react";
 
 const DashboardPage = () => {
   // Hooks
   const { userData, isLoading, error, refreshData } = useDashboardData();
 
+  // Refs
+  const loadingRef = useRef(null);
+  const loadedRef = useRef(null);
+
   return (
-    <div>
+    <TransitionGroup>
       {isLoading ? (
-        <LoadingDashboard />
+        <CSSTransition
+          unmountOnExit
+          nodeRef={loadingRef}
+          key={"loading"}
+          timeout={300}
+          classNames={"fade"}
+        >
+          <div ref={loadingRef}>
+            <LoadingDashboard />
+          </div>
+        </CSSTransition>
       ) : (
-        <>
-          {error !== undefined ? (
-            <ErrorDashboard handleRefresh={refreshData}/>
-          ) : (
-            <LoadedDashboard userData={userData} />
-          )}
-        </>
+        <CSSTransition
+          unmountOnExit
+          nodeRef={loadedRef}
+          key={"loaded"}
+          timeout={300}
+          classNames={"fade"}
+        >
+          <div ref={loadedRef}>
+            {error !== undefined ? (
+              <ErrorDashboard handleRefresh={refreshData} />
+            ) : (
+              <LoadedDashboard userData={userData} />
+            )}
+          </div>
+        </CSSTransition>
       )}
-    </div>
+    </TransitionGroup>
   );
 };
 
