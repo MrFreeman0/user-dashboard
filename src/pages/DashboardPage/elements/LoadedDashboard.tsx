ad46fa903f876d96ce13e7fb09f356393807ac0e
Ilia Maslovskii <32616894+MrFreeman0@users.noreply.github.com>
2024-10-10 14:39:25 +0300

    Implement styleless dashboard header

--- src/pages/DashboardPage/elements/LoadedDashboard.tsx
+++ src/pages/DashboardPage/elements/LoadedDashboard.tsx
@@ -1,4 +1,5 @@
 import INormalisedData from "../../../interfaces/INormsalisedData";
+import { TSortSelection, TSortBy } from "../../../interfaces/ISortInterface";
 import TNormalisedUsers from "../../../interfaces/TNormalisedUsers";
 import DashboardHeader from "../../../ui/DashboardHeader/DashboardHeader";
 import GridContainer from "../../../ui/GridContainer/GridContainer";
@@ -24,7 +25,12 @@ const LoadedDashboard = (props: IProps) => {
 
   return (
     <div>
-      <DashboardHeader />
+      <DashboardHeader
+        onQueryChange={handleQueryChange}
+        onSortSelectionChange={handleSortSelectionChange}
+        onSortByChange={handleSortByChange}
+        sortState={sortState}
+      />
       <GridContainer>
         {filteredAndSortedIds.map((id) => (
           <UserCard key={id} user={userData.data[id]} />
--- src/ui/DashboardHeader/DashboardHeader.tsx
+++ src/ui/DashboardHeader/DashboardHeader.tsx
@@ -1,6 +1,65 @@
+import { useMemo } from "react";
+import {
+  ISortState,
+  TSortBy,
+  TSortSelection,
+} from "../../interfaces/ISortInterface";
 import "./DashboardHeader.scss";
-const DashboardHeader = () => {
-  return <div>DashboardHeader</div>;
+import StyledInput from "../StyledInput/StyledInput";
+import StyledSelect from "../StyledSelect/StyledSelect";
+interface IProps {
+  onQueryChange: (query: string) => void;
+  onSortSelectionChange: (value: TSortSelection) => void;
+  onSortByChange: (value: TSortBy) => void;
+  sortState: ISortState;
+}
+
+const DashboardHeader = (props: IProps) => {
+  // Destructure
+  const { sortState, onQueryChange, onSortSelectionChange, onSortByChange } =
+    props;
+
+  //Memoized handlers
+  const handleQueryChange = useMemo(
+    () => (event: React.ChangeEvent<HTMLInputElement>) => {
+      onQueryChange(event.target.value);
+    },
+    [onQueryChange]
+  );
+
+  const handleSortSelectionChange = useMemo(
+    () => (event: React.ChangeEvent<HTMLSelectElement>) => {
+      onSortSelectionChange(event.target.value as TSortSelection);
+    },
+    [onSortSelectionChange]
+  );
+
+  const handleSortByChange = useMemo(
+    () => (event: React.ChangeEvent<HTMLSelectElement>) => {
+      onSortByChange(event.target.value as TSortBy);
+    },
+    [onSortByChange]
+  );
+
+  return (
+    <div>
+      <StyledInput
+        placeholder={"Search by name, email or address"}
+        onChange={handleQueryChange}
+      />
+      <StyledSelect
+        value={sortState.sortWith}
+        onChange={handleSortSelectionChange}
+      >
+        <option value={"name"}>Username</option>
+        <option value={"email"}>Email</option>
+      </StyledSelect>
+      <StyledSelect value={sortState.sortBy} onChange={handleSortByChange}>
+        <option value={"asc"}>Ascending</option>
+        <option value={"desc"}>Descending</option>
+      </StyledSelect>
+    </div>
+  );
 };
 
 export default DashboardHeader;
